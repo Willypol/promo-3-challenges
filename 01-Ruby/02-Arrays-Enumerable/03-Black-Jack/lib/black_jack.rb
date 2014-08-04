90b73a119a07a12c551da8b1321f568983077394
TODO: PUT YOUR NAME HERE WITHOUT ACCENTS <TODO: PUT YOUR GITHUB ACCOUNT EMAIL HERE>
2014-08-04 18:24:56 +0200

    my first solution

--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/black_jack.rb
@@ -1,11 +1,16 @@
 def bank_score
   # TODO: Use Random to get a new random score
+  a = 16 + rand(6)
+  return a
 end
 
 def pick_card
   # TODO: Use Random to get a new random card
+  b = 1+rand(11)
+  return b
 end
 
 def game_outcome(bank, score)
   # TODO: Take the bank and the score and output an array containing the bank and then the score
+  return outcome = [bank,score]
 end
--- 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
+++ 01-Ruby/02-Arrays-Enumerable/03-Black-Jack/lib/croupier.rb
@@ -2,16 +2,37 @@ require_relative 'black_jack'
 
 def play_game
   #TODO: make the user play from terminal in a while loop that will stop when the user will not be asking for  a new card
+  score = 0
+  query = asking_for_card?(score)
+  banque = bank_score
+  while query == true
+    score = score + pick_card
+    puts state_of_the_game(score, banque)
+    query = asking_for_card?(score)
+  end
+  outcome = game_outcome(banque, score)
+  puts build_message_for(outcome)
 end
 
 def state_of_the_game(score, bank)
   # TODO: Returns custom message with player's score and bank's score
+  return "Your score is #{score}, bank is #{bank}!"
 end
 
 def asking_for_card?(score)
   # TODO: Ask for a card only if the score is less or equal to 21, then returns true or false according to the user's choice
+  if score < 21
+    puts "Card ? (type 'Y' or 'yes' for a new card)"
+    resp = gets.chomp
+    condition = resp == "yes" || resp == "Y"
+    return condition
+  end
 end
 
 def build_message_for(outcome)
   #TODO: return specific message depending on the game outcome (= bank's score and user's score)
+  return "You are over 21... you loose." if outcome[1]>21
+  return "Black Jack!" if outcome[1] == 21
+  return "You beat the bank! You win." if outcome[1]>outcome[0]
+  return "Bank beats you! You loose." if outcome[1]<outcome[0]
 end
