f1c930e4a1c1358f80c27cbd62e45bacb78ceba7
TODO: PUT YOUR NAME HERE WITHOUT ACCENTS <TODO: PUT YOUR GITHUB ACCOUNT EMAIL HERE>
2014-08-05 12:37:17 +0200

    Destructive methods First try

--- 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
+++ 01-Ruby/02-Arrays-Enumerable/06-Destructive-methods/lib/pmu.rb
@@ -1,5 +1,9 @@
 def pmu_format!(race_array)
   #TODO: modify the given array so that it is PMU-consistent
+  race_array.map!.with_index do |pmu,index|
+    "#{index+1}-#{pmu}!"
+  end
+  race_array.reverse!
 end
 
 
