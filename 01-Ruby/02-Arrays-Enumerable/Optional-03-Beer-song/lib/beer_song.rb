ab05936bb8926ac69826d148096015f4cc211c2d
TODO: PUT YOUR NAME HERE WITHOUT ACCENTS <TODO: PUT YOUR GITHUB ACCOUNT EMAIL HERE>
2014-08-05 13:32:34 +0200

    beer_song first try

--- 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
+++ 01-Ruby/02-Arrays-Enumerable/Optional-03-Beer-song/lib/beer_song.rb
@@ -1,3 +1,18 @@
 def beersong(nb_at_start)
   #TODO: sing the song
+  while nb_at_start !=0
+    if nb_at_start > 1
+      puts "#{nb_at_start} bottles of beer on the wall, #{nb_at_start} bottles of beer!"
+      nb_at_start=nb_at_start-1
+      puts "Take one down, pass it around #{nb_at_start} bottles of beer on the wall!"
+    elsif nb_at_start==1
+      puts "#{nb_at_start} bottle of beer on the wall, #{nb_at_start} bottle of beer!"
+      nb_at_start=nb_at_start-1
+      puts "Take one down, pass it around no more bottles of beer on the wall!"
+    end
+  end
+end
+
+ARGV.each do |a|
+  beersong(a.to_i)
 end
