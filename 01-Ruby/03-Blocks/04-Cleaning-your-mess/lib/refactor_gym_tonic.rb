95b51dc31f1b95004ced5956a1b0cde6ff53244b
TODO: PUT YOUR NAME HERE WITHOUT ACCENTS <TODO: PUT YOUR GITHUB ACCOUNT EMAIL HERE>
2014-08-05 14:37:57 +0200

    First refacto

--- 01-Ruby/03-Blocks/04-Cleaning-your-mess/lib/refactor_gym_tonic.rb
+++ 01-Ruby/03-Blocks/04-Cleaning-your-mess/lib/refactor_gym_tonic.rb
@@ -1,18 +1,29 @@
 # Very dirty code to make some dirty gym...
-def hop_hop_hop(number_of_exercises)
-  for i in (1..number_of_exercises) do 
-    counter = 0
-    until counter == i do
+# def hop_hop_hop(number_of_exercises)
+#   for i in (1..number_of_exercises) do
+#     counter = 0
+#     until counter == i do
+#       print "hop! "
+#       counter += 1
+#     end
+#     unless counter != i # Test if reaching the end of the current exercise.
+#       # Prints message in that case
+#       print 'One more time..' + "\n"
+#     end
+#     # Reset counter to 0 for the next exercise
+#     counter = 0
+#   end
+# end
+
+def hop_hop_hop_2(number_of_exercises)
+  i = 1
+  until i > number_of_exercises do
+    for j in (1..i) do
       print "hop! "
-      counter += 1
     end
-    unless counter != i # Test if reaching the end of the current exercise.
-      # Prints message in that case    
-      print 'One more time..' + "\n"
-    end  
-    # Reset counter to 0 for the next exercise
-    counter = 0
+    print 'One more time..' + "\n"
+    i+=1
   end
 end
 
-hop_hop_hop(6)
+hop_hop_hop_2(6)
