1f76b5cc9d08dad5ce84b5aba29e2591f1e207e6
TODO: PUT YOUR NAME HERE WITHOUT ACCENTS <TODO: PUT YOUR GITHUB ACCOUNT EMAIL HERE>
2014-08-05 17:34:44 +0200

    code du préfixe

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -2,4 +2,17 @@
 
 def louchebemize(sentence)
   #TODO: implement your louchebem translator
+  puts arrange_beginning(sentence)
 end
+
+def arrange_beginning(word)
+  unless word.start_with?("a","e","i","o","u")
+    word=word+word[0] #word.split("").last
+    word=word[1..-1]
+    arrange_beginning(word)
+  end
+  word = "l"+word
+  return word
+end
+
+louchebemize("saluté")
