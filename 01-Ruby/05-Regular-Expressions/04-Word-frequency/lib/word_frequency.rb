035ed667a18707225c49b82d9150f19ff5e197fa
TODO: PUT YOUR NAME HERE WITHOUT ACCENTS <TODO: PUT YOUR GITHUB ACCOUNT EMAIL HERE>
2014-08-07 17:33:41 +0200

    frequency pas stop words implémentés

--- 01-Ruby/05-Regular-Expressions/04-Word-frequency/lib/word_frequency.rb
+++ 01-Ruby/05-Regular-Expressions/04-Word-frequency/lib/word_frequency.rb
@@ -4,14 +4,19 @@ def most_common_words(file_name, stop_words_file_name, number_of_word)
 
   File.open(file_name,"r").each_line do |line|
     words_array = line.split
+    temp_array = []
     words_array.each do |word|
+      count = 0
       File.open(stop_words_file_name,"r").each_line do |stop_word|
         if stop_word==word
-          #get rid of word, break the loop
-          words_array.delete(word)
-          break
+          count += 1
         end
       end
+      if count == 0
+        temp_array << word
+      end
+    end
+    temp_array.each do |word|
       if hash_words[word] != nil
         hash_words[word] += 1
       else
@@ -19,7 +24,8 @@ def most_common_words(file_name, stop_words_file_name, number_of_word)
       end
     end
   end
-  puts hash_words
+  puts hash_words.sort_by{|_key, value| value}.reverse.first(number_of_word)
+
 end
 
 most_common_words("testWagon.txt","stop_words.txt",5)
