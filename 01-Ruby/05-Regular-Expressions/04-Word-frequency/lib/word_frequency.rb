333ef7cb8955fd97ce1d2647e167e175343dc764
TODO: PUT YOUR NAME HERE WITHOUT ACCENTS <TODO: PUT YOUR GITHUB ACCOUNT EMAIL HERE>
2014-08-07 16:57:05 +0200

    frequency counting first try

--- 01-Ruby/05-Regular-Expressions/04-Word-frequency/lib/word_frequency.rb
+++ 01-Ruby/05-Regular-Expressions/04-Word-frequency/lib/word_frequency.rb
@@ -1,3 +1,25 @@
 def most_common_words(file_name, stop_words_file_name, number_of_word)
   #TODO: return hash of occurences of number_of_word most frequent words
+  hash_words = Hash.new()
+
+  File.open(file_name,"r").each_line do |line|
+    words_array = line.split
+    words_array.each do |word|
+      File.open(stop_words_file_name,"r").each_line do |stop_word|
+        if stop_word==word
+          #get rid of word, break the loop
+          words_array.delete(word)
+          break
+        end
+      end
+      if hash_words[word] != nil
+        hash_words[word] += 1
+      else
+        hash_words[word] = 1
+      end
+    end
+  end
+  puts hash_words
 end
+
+most_common_words("testWagon.txt","stop_words.txt",5)
