986fb81a69d7eb53ba00714bf3d7c3a3c9128964
TODO: PUT YOUR NAME HERE WITHOUT ACCENTS <TODO: PUT YOUR GITHUB ACCOUNT EMAIL HERE>
2014-08-07 15:49:58 +0200

    regex 1

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,9 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  if phone_number =~ /^(0|\+33)(1|6|\s6)(\d{8}|(\s\d{2}){4}|(\-\d{2}){4})/
+    return true
+  else
+    return false
+  end
+
 end
