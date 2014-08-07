dbe8a2c90a20aa36c8e5da42c38f2c8a39ccd013
TODO: PUT YOUR NAME HERE WITHOUT ACCENTS <TODO: PUT YOUR GITHUB ACCOUNT EMAIL HERE>
2014-08-07 18:02:17 +0200

    accessors filled

--- 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/playboy.rb
+++ 02-OOP/01-OO-Basics/02-Getters-and-Setters/lib/playboy.rb
@@ -2,13 +2,20 @@ class Playboy
 
   #TODO: implement necessary getters or setters to run meeting_casanova.rb
 
+  attr_accessor:nationality
+  attr_accessor:conquests
+  attr_accessor:hair_length
+
   INITIAL_HAIR_LENGTH = 20  # beautiful long playboy hair
 
   def initialize(name, nationality)
     @conquests = []
-    @name, @nationality = name, nationality
+    @name = name
+    @nationality = nationality
     @hair_length = INITIAL_HAIR_LENGTH
   end
+
+
 
   def meets(lady)
     @conquests << lady unless @married
