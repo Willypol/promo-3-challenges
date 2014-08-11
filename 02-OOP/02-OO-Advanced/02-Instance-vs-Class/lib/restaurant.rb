1da0668ca6bd6538bea26089f17720ae3c8df167
TODO: PUT YOUR NAME HERE WITHOUT ACCENTS <TODO: PUT YOUR GITHUB ACCOUNT EMAIL HERE>
2014-08-11 12:17:59 +0200

    exo 02 instance vs class fini

--- 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
@@ -1,11 +1,23 @@
 class Restaurant
-  
+  attr_accessor :name, :adress, :type_of_food, :city, :average_rating
   #TODO add relevant accessors if necessary
-  
+
   def initialize(city, name)
     #TODO: implement constructor with relevant instance variables
+    @name = name
+    @city = city
+    @adress = "to be defined"
+    @type_of_food = "to be defined"
+    @average_rating = 0
+    @average_rating_coef = 0
   end
-  
-  #TODO: implement #filter_by_city and #rate methods
-  
+
+  def rate(rating)
+    @average_rating_coef+=1
+    @average_rating = (@average_rating+rating)/(@average_rating_coef)
+  end
+
+  def self.filter_by_city(restaurants, city)
+    return restaurants.select {|restaurant| restaurant.city == city}
+  end
 end
